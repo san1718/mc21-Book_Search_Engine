261ceb8af50a4c1c8a1d8dde5c4288b2894e3134
Sung Min An <an.sungmin17@icloud.com>
2025-01-20 18:53:52 -0800

    clientsignup

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { ADDUSER } from '../utils/mutations';
+import { userQuery, useMutation } from '@apollo/client';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ 
+    username: '', 
+    email: '', 
+    password: '',
+  });
+
   // set state for form validation
   const [validated] = useState(false);
+
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
@@ -28,14 +36,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      const token = data.addUser.token;
       Auth.login(token);
     } catch (err) {
       console.error(err);
