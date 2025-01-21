6a34549cd0d0a7c1d015ae9c1dbd944766cef367
Sung Min An <an.sungmin17@icloud.com>
2025-01-20 18:27:02 -0800

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_,  { headers }) => {
+  // This will get the authentication token from the localStorage if any
+  const token = localStorage.getItem('id_token');
+  // Returns headers to the httpLink to be read
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
