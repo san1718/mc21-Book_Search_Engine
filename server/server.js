bbd6a677015aa99b7cfb87765be3bd12599b9c0b
Sung Min An <an.sungmin17@icloud.com>
2025-01-30 20:26:59 -0800

    deploy

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const startApolloserver = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req,  res) => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
